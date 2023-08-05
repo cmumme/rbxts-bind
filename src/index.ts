00bb6ffc2ae7fb0066dff56f8e82a05d171f47bd
TypeChecked <camrenmumme@gmail.com>
2023-08-05 14:55:25 -0400

    Loader - Fix bugs

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,24 @@
 */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
+import { Reflect } from "@rbxts/experimental-reflect"
+import Object from "@rbxts/object-utils"
+
 // Types
 type UnknownFunction = (...args: any) => any
+type Methodify<T extends UnknownFunction, K extends object = { }> = (this: K, ...Arguments: Parameters<T>) => void
 
-type GetSignalGeneric<C extends Signal<never>> = C extends Signal<infer T> ? T : unknown
+type GetSignalGeneric<C extends Signal<any>> = C extends Signal<infer T> ? T : unknown
 
 interface Signal<T extends UnknownFunction = UnknownFunction> {
-	Connect: (func: T) => RBXScriptConnection,
-	Once: (func: T) => RBXScriptConnection,
-	Wait: () => any
+	Connect: (this: Signal<T>, func: T) => RBXScriptConnection,
+	ConnectParallel: (this: Signal<T>, func: T) => RBXScriptConnection,
+	Once: (this: Signal<T>, func: T) => RBXScriptConnection,
+	Wait: (this: Signal<T>) => any
+}
+
+interface MethodDescriptor {
+	dd: string
 }
 
 /**
@@ -24,8 +33,32 @@ interface Signal<T extends UnknownFunction = UnknownFunction> {
  * @param Signal The signal to bind this method to.
  * @returns Method Decorator
  */
-export const BindMethod = <C extends Signal, T extends UnknownFunction = GetSignalGeneric<C>>(Signal: C) => {
-	return <K extends string, V extends { [Key in K]: T }>(Target: V, Key: K, __: unknown) => {
-		Signal.Connect(Target[Key])
+export const BindMethod = <C extends Signal, T extends Methodify<UnknownFunction> = Methodify<GetSignalGeneric<C>>>(Signal: C) => {
+	return <K extends string, V extends { [Key in K]: T }>(Target: V, Key: K, _: unknown) => {
+		Reflect.defineMetadata("ts:bind:bindedsignal", Signal, Target, Key)
+	}
+}
+
+export namespace Bind {
+	export function Activate(Target: object) {
+		const Metatable = getmetatable(Target) as object | undefined
+		const Keys = Object.keys(
+			Object.assign({ },
+				Target,
+				Metatable && Metatable,
+				Metatable && "__index" in Metatable && Metatable["__index"]
+			)
+		) as unknown as string[] // If we're working with a class we need to add any metamethods or properties from the class instance's metatable.
+
+		Keys.forEach((Key: string) => {
+			if(!(Key in Target)) return
+			const BindedSignal = Reflect.getMetadata("ts:bind:bindedsignal", Target, Key) as RBXScriptSignal | undefined
+			const BindedMethod = (Target as Record<string, any>)[Key] as UnknownFunction // We don't methodify as we have to manually pass through this later down the line.
+			if(BindedSignal === undefined) return
+
+			BindedSignal.Connect((...Args: unknown[]) => {
+				BindedMethod(Target, ...Args)
+			})
+		})
 	}
 }
